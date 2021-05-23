3a54436a567d735df44a0059bae6618396957c9c
SirZempo <49045842+SirZempo@users.noreply.github.com>
2021-05-23 12:54:50 -0500

    Se crearon las funciones de Baraja
    Se crearon las funciones de ObtenerBaraja y RevolverBaraja

--- memorama/src/Baraja.java
+++ memorama/src/Baraja.java
@@ -1,7 +1,38 @@
+import java.util.Arrays;
+import java.util.List;
+
+
 /**
  *
- * @author 
+ * @author Uriel Jimenez Zempoalteca
  */
+
 public class Baraja {
+    private Cartas[] cartas;
+    private Imagenes[] img; 
+    
+    public Baraja()
+    {
+        ObtenerCartas();
+        RevolverCartas();
+    }
+    
+    public void ObtenerCartas()
+    {
+        //Obteniendo cartas
+        String[] imagen = img.getImagenes();
+        cartas = new Cartas[img.getCantidadImagenes()*2];
+    }
+    
+    public void RevolverCartas()
+    {
+        //Revolviendo Cartas
+        
+    }
+    
+    public Cartas[] getCartas()
+    {
+        return cartas; 
+    }
     
 }
