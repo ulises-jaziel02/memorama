4b36390aad0aa208be824396a53ccf0ccb6df932
DiegoToledoA <84341287+DiegoToledoA@users.noreply.github.com>
2021-05-23 18:33:52 -0500

    Update Jugador.java

--- memorama/src/Jugador.java
+++ memorama/src/Jugador.java
@@ -1,7 +1,41 @@
-/**
- *
- * @author 
- */
-public class Jugador {
+ public class Jugador {
+    private  int puntaje;
+    private  boolean turno;
+    private String nombre;
+
+    public Jugador(boolean turno) {
+        this.puntaje = 0;
+        this.turno = turno;
+    }
+    
+    public  void sumaPunto() {
+        puntaje++;
+    }
+
+    public  int getPuntaje() {
+        return puntaje;
+    }
+
+    public  void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }  
+
+    public  boolean isTurno() {
+        return turno;
+    }
+
+    public  void setTurno(boolean turno) {
+       this.turno = turno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
     
 }
